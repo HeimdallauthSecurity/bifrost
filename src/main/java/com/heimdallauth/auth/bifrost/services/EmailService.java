d3bb60ee4e421bfe5f0ed1bfe4d1a288e7309fc5
Mayank Soni <106593569+mayanksoni1996@users.noreply.github.com>
2024-08-28 22:18:54 -0600

    imrpove(email-body): HSB-9 Implement a method to trigger body generation

--- src/main/java/com/heimdallauth/auth/bifrost/services/EmailService.java
+++ src/main/java/com/heimdallauth/auth/bifrost/services/EmailService.java
@@ -6,14 +6,18 @@ import com.heimdallauth.auth.bifrost.dao.EmailDataManager;
 import com.heimdallauth.auth.bifrost.dao.EmailDocument;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
 
 @Service
 public class EmailService {
     private final EmailDataManager emailDataManager;
+    private final TemplateEngine templateEngine;
 
     @Autowired
-    public EmailService(EmailDataManager emailDataManager) {
+    public EmailService(EmailDataManager emailDataManager, TemplateEngine templateEngine) {
         this.emailDataManager = emailDataManager;
+        this.templateEngine = templateEngine;
     }
     public EmailDocument createEmailDeliveryRecord(String emailAddress, String profileId, MailType mailType){
         EmailDocument emailDocument = EmailDocument.builder()
@@ -25,5 +29,10 @@ public class EmailService {
     }
     public EmailDocument updateEmailDeliveryStatus(String emailDocumentId, DeliveryStatus deliveryStatus){
         return emailDataManager.updateEmailDeliveryStatus(emailDocumentId, deliveryStatus);
+    }
+    public String generateEmailBody(String username){
+        Context context = new Context();
+        context.setVariable("username", username);
+        return templateEngine.process("email-template", context);
     }
 }
